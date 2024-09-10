39159450dc599ac731e6845059e0cacfa1ee5254
Honour Sigbeku <sigbekuhonour@gmail.com>
2024-09-10 17:43:52 -0230

    first commit

--- src/app/Contact/Contact.tsx
+++ src/app/Contact/Contact.tsx
@@ -1,17 +1,20 @@
-function Info({ task, value }:{task:React.ReactNode,value:React.ReactNode}) {
-    return (
-      <div className="flex flex-col justify-evenly">
-        <h1 className="font-normal ml-3">{task}</h1>
-        <h2 className="font-medium ml-3">{value}</h2>
-      </div>
-    );
-  }
-  
-export default function Contact(){
-    return(
-        <div id="Contact" className="flex flex-row justify-between mt-4 items-start border-t-4 border-black">
-            <Info task={"Phone"} value={"+1(709)-853-2123"} />
-            <Info task={"Email"} value={"sigbekuhonour@gmail.com"} />
-        </div>
-    );
+function Info({ task, value }: any) {
+  return (
+    <div className="flex flex-col justify-evenly">
+      <h1 className="font-normal ml-3">{task}</h1>
+      <h2 className="font-medium ml-3">{value}</h2>
+    </div>
+  );
+}
+
+export default function Contact() {
+  return (
+    <div
+      id="Contact"
+      className="flex flex-row justify-between mt-4 items-start border-t-4 border-black"
+    >
+      <Info task={"Phone"} value={"+1(709)-853-2123"} />
+      <Info task={"Email"} value={"sigbekuhonour@gmail.com"} />
+    </div>
+  );
 }
