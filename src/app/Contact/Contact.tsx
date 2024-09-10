48db351743d04cc4cca5e7dbe3a6021b0e984fc4
Honour Sigbeku <sigbekuhonour@gmail.com>
2024-09-10 17:56:23 -0230

    first commit

--- src/app/Contact/Contact.tsx
+++ src/app/Contact/Contact.tsx
@@ -1,7 +1,7 @@
 function Info({ task, value }: any) {
   return (
     <div className="flex flex-col justify-evenly">
-      <h1 className="font-normal ml-3 px-8">{task}</h1>
+      <h1 className="font-normal ml-3">{task}</h1>
       <h2 className="font-medium ml-3">{value}</h2>
     </div>
   );
@@ -11,7 +11,7 @@ export default function Contact() {
   return (
     <div
       id="Contact"
-      className="flex flex-row justify-center mt-4 items-start border-t-4 border-black"
+      className="flex flex-row justify-evenly mt-4 items-start border-t-4 border-black"
     >
       <Info task={"Phone"} value={"+1(709)-853-2123"} />
       <Info task={"Email"} value={"sigbekuhonour@gmail.com"} />
